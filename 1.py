e88edbfcff049e9acfe6bee7b2cf93896c6a12d0
RIP1982 <perovsky@list.ru>
2022-09-19 22:04:39 +0400

    Solution for task 1

---
+++ 1.py
@@ -0,0 +1,6 @@
+# Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+string = 'Я очнулся от забвения в Зимбабве под звуки горна!'
+letters = 'абв'
+new_string = list(filter(lambda x: letters not in x, string.split()))
+print(' '.join(new_string))
