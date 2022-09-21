f50979af53de52b3e2578ce0914b1896704637af
RIP1982 <perovsky@list.ru>
2022-09-21 18:25:48 +0400

    Solution for task 1 with corrective

--- 1.py
+++ 1.py
@@ -3,4 +3,5 @@
 string = 'Я очнулся от забвения в Зимбабве под звуки горна!'
 letters = 'абв'
 new_string = list(filter(lambda x: letters not in x, string.split()))
+print(string)
 print(' '.join(new_string))
